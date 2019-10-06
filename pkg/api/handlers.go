44b69dd3fd719c5b4f1f6b3d664a1d3eedce6233
Ricki Hastings <rickihastings22@gmail.com>
2019-10-06 10:45:22 +0100

    Implementing all store API functions

--- pkg/api/handlers.go
+++ pkg/api/handlers.go
@@ -2,8 +2,17 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
+
+func internalServerError(w http.ResponseWriter, logMessage string) {
+	fmt.Println(logMessage)
+	http.Error(w, http.StatusText(500), 500)
+}
 
 func addItemHandler(w http.ResponseWriter, r *http.Request) {
 	// Try and parse this into the struct, this code could be reusable but golang doesn't have generics
@@ -21,17 +30,55 @@ func addItemHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte(http.StatusText(200)))
+	// Add the item to our store
+	err = addItemToStore(request)
+	if err != nil {
+		internalServerError(w, "ERROR: Cannot add item to store")
+	} else {
+		w.Write([]byte(http.StatusText(200)))
+	}
 }
 
 func removeItemHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	// Try and get the id from the router, and convert it to an integer
+	variables := mux.Vars(r)
+	id, err := strconv.Atoi(variables["id"])
+	if err != nil {
+		http.Error(w, "Bad Request: ID should be a number", 400)
+		return
+	}
+
+	err = removeItemFromStore(id)
+	if err != nil {
+		internalServerError(w, "ERROR: Cannot remove item from store")
+	} else {
+		w.Write([]byte(http.StatusText(200)))
+	}
 }
 
 func clearItemsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	err := clearStore()
+	if err != nil {
+		internalServerError(w, "ERROR: Cannot clear store")
+	} else {
+		w.Write([]byte(http.StatusText(200)))
+	}
 }
 
 func listItemsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	w.Header().Set("Content-Type", "application/json")
+
+	items, err := getItemsFromStore()
+	if err != nil {
+		internalServerError(w, "ERROR: Cannot get items from store")
+		return
+	}
+
+	// We need to convert the items to JSON so they can be transferred over HTTP.
+	bytes, err := json.Marshal(items)
+	if err != nil {
+		internalServerError(w, "ERROR: Cannot stringify items in store")
+	} else {
+		w.Write(bytes)
+	}
 }
---
+++ pkg/api/store.go
@@ -0,0 +1,59 @@
+package api
+
+type item struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Price int    `json:"price"`
+}
+
+// This is an in memory database, all interactions with it are abstracted behind functions here
+// If we were to convert this to a persistent database, we should only need to change this interface
+// The functions all return error because of this, as our interface is just an in memory db it can't actually error
+// But the handlers are setup to handle this state
+var items []item
+
+func init() {
+	items = []item{}
+}
+
+func indexOf(id int, data []item) int {
+	for k, v := range data {
+		if id == v.ID {
+			return k
+		}
+	}
+
+	return -1
+}
+
+func addItemToStore(request addItemRequest) error {
+	id := len(items) + 1
+	items = append(items, item{
+		ID:    id,
+		Title: request.Title,
+		Price: request.Price,
+	})
+
+	return nil
+}
+
+func removeItemFromStore(id int) error {
+	index := indexOf(id, items)
+	if index != -1 {
+		// As I'd like to keep the items ordered, in Golang we have to shift all of the elements at
+		// the right of the one being deleted, by one to the left.
+		items = append(items[:index], items[index+1:]...)
+	}
+
+	return nil
+}
+
+func clearStore() error {
+	items = []item{}
+
+	return nil
+}
+
+func getItemsFromStore() ([]item, error) {
+	return items, nil
+}
